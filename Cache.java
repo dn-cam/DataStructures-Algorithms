60d09e0b93438ed8860fc927c3130c0d9c39cb55
Camellia Debnath <debnathc1@gmail.com>
2019-04-04 23:32:49 -0400

    LRU Cache

---
+++ Cache.java
@@ -0,0 +1,60 @@
+/**
+ * 
+ */
+package husky.neu.edu;
+
+import java.util.*;
+
+/**
+ * @author camelliadebnath 04-Apr-2019
+ */
+
+class DLL {
+	String site;
+	DLL prev;
+	DLL next;
+
+	DLL(String site) {
+		this.site = site;
+		this.prev = null;
+		this.next = null;
+	}
+}
+
+public class Cache {
+	static DLL cacheHead = new DLL("");;
+	static Map<String, DLL> map = new HashMap<String, DLL>();;
+	static int CACHE_SIZE;
+	static int counter = 1;
+
+	public Cache(int n) {
+		CACHE_SIZE = n;
+	}
+
+	/**
+	 * checks if thiis site exists in the hashmap if it exists, then just display
+	 * it, and put it at the head of the DLL if it doesn't, add it to the hashmap,
+	 * and put it at the head of the DLL
+	 */
+	public String access(String sitename) {
+		if (Cache.map.containsKey(sitename)) {
+			if (Cache.cacheHead.site != sitename) {
+				DLL temp = Cache.cacheHead;
+				DLL site = Cache.map.get(sitename);
+				site.next = Cache.cacheHead;
+				Cache.cacheHead.prev = site;
+				Cache.cacheHead = temp;
+				temp.prev = null;
+			}
+		} else {
+			if(Cache.counter < Cache.CACHE_SIZE) {
+				
+				
+			}
+		}
+
+		return Cache.cacheHead.site;
+
+	}
+
+}
